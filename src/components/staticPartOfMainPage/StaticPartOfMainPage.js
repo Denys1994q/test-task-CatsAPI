3a6351960fa55e948e7546523dc7450542b955a4
Denys1994q <drybachok@gmail.com>
2022-08-11 21:12:53 +0300

    val

--- src/components/staticPartOfMainPage/StaticPartOfMainPage.js
+++ src/components/staticPartOfMainPage/StaticPartOfMainPage.js
@@ -30,6 +30,7 @@ const StaticPartOfMainPage = (props) => {
     return (
         <div className="home">
             <div>1</div>
+            <div>2</div>
             <div className="home-wrapper">
                 <div className="home-right">
                     <div className="sticky">
